4ddf2729533b7f253471d96fbfed922c6a539a86
AndersonCMelo <andersonmelo13@gmail.com>
2020-06-13 14:22:16 -0300

    First commit

---
+++ js/accordion.js
@@ -0,0 +1,59 @@
+function action1() {
+  document
+      .querySelector('.answer1')
+      .classList
+      .toggle('hide');
+    
+  document
+    .querySelector('.accordion-header1')
+    .classList
+    .toggle('change-icon');
+}
+
+function action2() {
+  document
+      .querySelector('.answer2')
+      .classList
+      .toggle('hide');
+    
+  document
+    .querySelector('.accordion-header2')
+    .classList
+    .toggle('change-icon');
+}
+
+function action3() {
+  document
+      .querySelector('.answer3')
+      .classList
+      .toggle('hide');
+    
+  document
+    .querySelector('.accordion-header3')
+    .classList
+    .toggle('change-icon');
+}
+
+function action4() {
+  document
+      .querySelector('.answer4')
+      .classList
+      .toggle('hide');
+    
+  document
+    .querySelector('.accordion-header4')
+    .classList
+    .toggle('change-icon');
+}
+
+function action5() {
+  document
+      .querySelector('.answer5')
+      .classList
+      .toggle('hide');
+    
+  document
+    .querySelector('.accordion-header5')
+    .classList
+    .toggle('change-icon');
+}
